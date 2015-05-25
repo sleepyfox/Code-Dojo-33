c75cc9233fdcde5b738a80cc28e95eea23efa65f
Fox <sleepyfox@gmail.com>
2015-05-25 22:07:15 +0100

    [feature] added test for find-last-space to cover trailing whitespace

--- test-wrap.lisp
+++ test-wrap.lisp
@@ -1,4 +1,8 @@
+;; Imports
 (var tap (require "tap"))
+
+;; Constants
+(var NOTFOUND -1) ; for use with String.indexOf
 
 (def wrap-at (column string)
 	(if-else 
@@ -8,12 +12,16 @@
 			(+ (string.substr 0 column) "\n" (string.substr column))
 		string))
 
-(def find-last-space (string)
-	[string ""])
+(def find-last-space (s)
+	(var string (s.trimRight))
+	(var space (string.indexOf " "))
+	(if (= space NOTFOUND)
+		[string ""]
+		[(string.substr 0 space) (string.substr (+ 1 space))]))
 
 
 (tap.test "A space hunter" (lambda (suite)
-	(suite.plan 2)
+	(suite.plan 4)
 	(suite.test "when given an empty string" (lambda (t)
 		(t.plan 1)
 		(t.same (find-last-space "") ["" ""] 
@@ -26,7 +34,11 @@
 		(t.plan 1)
 		(t.same (find-last-space "lol cat") ["lol" "cat"]
 			"should return the two words as strings")))
-	))
+	(suite.test "when given a string with trailing whitespace" (lambda (t)
+		(t.plan 1)
+		(t.same (find-last-space "lol  ") ["lol" ""]
+			"should return the same string with no trailing spaces")))
+))
 
 (tap.test "A line wrapper" (lambda (suite)
 	(suite.plan 3)
