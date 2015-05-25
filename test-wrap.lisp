24e33e7b9ad723405ef062abb5a0dc85236a0869
Fox <sleepyfox@gmail.com>
2015-05-25 19:25:36 +0100

    [refactor] to remove hard-coded answer

--- test-wrap.lisp
+++ test-wrap.lisp
@@ -3,9 +3,9 @@
 (def wrap-at (column string)
 	(if (= string "") 
 		"" 
-		(if (= column 3) 
-			"lol\nlol"
-			string)))
+		(if (> column string.length) 
+			string
+			(+ (string.substr 0 column) "\n" (string.substr column)))))
 
 (tap.test "A line wrapper" (lambda (suite)
 	(suite.test "should not wrap an empty string" (lambda (t)
@@ -18,6 +18,7 @@
 		(t.equal (wrap-at 10 "otter") "otter" "got back the word unchanged")
 		(t.end)))
 	(suite.test "should wrap run on text with no spaces" (#(t)
-		(t.plan 1)
-		(t.equal (wrap-at 3 "lollol") "lol\nlol" "wrapped correctly")
+		(t.plan 2)
+		(t.equal (wrap-at 3 "lollol") "lol\nlol" "wrapped after lol")
+		(t.equal (wrap-at 4 "boldcat") "bold\ncat" "wrapped after bold")
 		(t.end)))))
